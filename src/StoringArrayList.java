eca222b142cf66cc1a93f879cecfbe827ee851e4
Tonia <tonia.saba@gmail.com>
2022-01-02 16:28:03 -0500

    First commit

---
+++ src/StoringArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class StoringArrayList {
+
+	public static void main(String[] args) {
+	// Student object ArrayList, This will store  
+	// that will store 10 student objects in an ArrayList, ArrayList<Student>
+        ArrayList<Student>studentArrayList = new ArrayList<Student>();
+
+	
+	}
+
+}
